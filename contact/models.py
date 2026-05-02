540b988bdd6c217ce8ad6dde0253ff1c6ca18d43
Guilherme Nunes <gui_augustonunes@outlook.com>
2026-05-02 00:06:15 -0300

    Adicionando model Owner também com uma ForeignKey

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,6 +11,10 @@ from django.utils import timezone
 #picture(image)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -25,6 +30,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/$Y/$m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
