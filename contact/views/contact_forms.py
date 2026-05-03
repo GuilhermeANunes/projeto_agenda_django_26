725b856158ff6bbdb850e96602d5d332b9d485e9
Guilherme Nunes <gui_augustonunes@outlook.com>
2026-05-03 03:37:55 -0300

    Adicionando forms no Create

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.db.models import Q
-from django.core.paginator import Paginator
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 # Create your views here.
 def create(request):
 
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST),
+            'site_title': 'Create - ',
+        }
+
+        return render (
+            request,
+            'contact/create.html',
+            context,
+        )
+
     context = {
-        'page_obj': '',
+        'form': ContactForm,
         'site_title': 'Create - ',
     }
 
