7b81be919c89b003651f00c4583b770cc1030ef3
Guilherme Nunes <gui_augustonunes@outlook.com>
2026-05-03 01:33:36 -0300

    Criando partials e configurando search com Q e OR

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,6 @@ from contact.views import index_view
 
 urlpatterns = [
     path('<int:contact_id>/', index_view.contact, name='single_contact'),
+    path('search/', index_view.search, name='search'),
     path('', index_view.index, name='index'),
 ]
